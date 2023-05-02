78521b49edd4a93333ac55d379ef4ba002e9832a
Kaew Tibkham <kaewtibkham@gmail.com>
2023-05-02 13:39:09 +0700

    Update map

--- src/main/java/com/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,13 @@ package com.example.fizzbuzz;
 public class FizzBuzz {
 
     public static String map(int i) {
-        if (i ==3) {
+
+        if (i == 5) {
+            return "Buzz";
+        }
+        if (i == 3) {
             return "Fizz";
         }
-        return ""+i;
+        return "" + i;
     }
 }
